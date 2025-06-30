5a6e035cfb5a127dd842cbb7ff32bb33e9addae7
Dryw Wade <dryw.wade@sparkfun.com>
2025-06-30 17:20:43 -0600

    Initial readme and example code

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# xrp_balance_bot
+# XRP Balance Bot
+
+This repository documents the XRP Balance Bot! A modification to the base XRP robot with a simple hardware change that opens up a brand new project requiring new software and control theory concepts with numerous expansion possibilities!
+
+![img](images/xrp_balance_bot.jpg)
+
+# Hardware
+
+The only things you need are:
+
+* The base XRP Kit: https://www.sparkfun.com/experiential-robotics-platform-xrp-kit.html
+    * You will need to remove the servo or mount it elsewhere, because it would otherwise hit the ground.
+* Bigger wheels: https://www.printables.com/model/1342650-xrp-balance-bot
+    * Any wheels with a 4" diameter or larger should work just fine!
+
+# Software
+
+[Example code](code/balance_bot.py) is provided if you just want to get your robot balancing right away! Though it's also fun to write your own code from scratch to exercise your control theory and programming skills!
+
+# Expansion Ideas
+
+Below are some ideas of how to expand this project to do more:
+
+* Remote control
+    * The example code will simply try to keep the robot upright and maintain position. However, it would be awesome to be able to remotely drive the robot with a gamepad!
+    * The example code already has placeholders for this feature, take a look at the `target_speed` and `turn_effort` variables. You'll need to modify the code to set these based on gamepad inputs. What cool tricks can you do with your balance bot?
+* Deceleration
+    * The example code simply uses a PID controller for the target pitch angle. This means when the robot is driving towards its target location, it doesn't slow down nearly soon enough, resulting in a large overshoot of the position.
+    * To make the robot stop at the target position, the robot needs to lean back to slow down before it reaches the target. You'll need to change the angle PID controller to something that can look ahead in this way to slow down in time. How accurately can your balance bot drive?
+* Re-mount the sensors
+    * The XRP includes an ultrasonic distance sensor and a line sensor. The normal clips for those don't really work with the balance bot, so it would be great to design new clips for this new orientation of the XRP!
+    * The ultrasonic sensor needs to point forward to see walls and other objects and measure the distance to them. Should it be at the same height, or a different height?
+    * The line sensor needs to be close to the gound to see lines. Can you make your XRP balance bot follow a line?
+* Mount the servo arm
+    * This project requires removing the servo, because the normal location for the clip hits the ground. It would need to be mounted somewhere else with a new clip design to make it useful again.
+    * Design a new servo clip the can be used to pick up objects, or even a gripper! What can you pick up with your balance bot?
---
+++ code/balance_bot.py
@@ -0,0 +1,110 @@
+# Import libraries
+from XRPLib.defaults import *
+from XRPLib.pid import PID
+import time
+
+# Wheel diameter in cm
+drivetrain.wheel_diam = 10
+
+# Loop period in seconds. The main loop needs to run relatively fast to ensure
+# the PID controllers can react quickly
+loop_period = 0.01
+
+# Create PID controller that maintains the robot's pitch angle by actuating the
+# motors
+angle_pid = PID(
+    kp = 0.1, # 0.1 corresponds to full effort per 10 degrees
+    ki = 0.25, # 0.25 corresponds to full effort per 4 degree seconds
+    kd = 0.005, # 0.005 corresponds to full effort per 200 degrees per second
+    
+    # Although the motor's max effort is 1, let the PID controller's output be
+    # larger to detect when it goes extremely high, which likely indicates the
+    # robot has fallen over
+    max_output = 10
+)
+
+# Create PID controller that maintains the robot's position by adjusting the
+# target pitch angle
+position_pid = PID(
+    kp = -0.1, # 0.1 corresponds to 1 degree per 10 cm
+    ki = -0.025, # 0.025 corresponds to 1 degree per 40 cm seconds
+    kd = -0.025, # 0.025 corresponds to 1 degree per 40 cm per second
+
+    # Although the angle of the robot will never realistically be more than a
+    # few degrees, let the PID controller's output be a lot larger to give
+    # plenty of margin
+    max_output = 90
+)
+
+# Loop forever, allowing the user to stand up the robot and start balancing
+# over and over again
+while(True):
+    # Inform user to stand up the robot
+    print("Ready, stand up robot!")
+    board.set_rgb_led(0,64,0)
+
+    # Wait for robot to stand. The balancing point is roughly straight up, at
+    # which point the z-axis of the accelerometer should be around 0. So we'll
+    # wait until it's within 10 milli-g of zero to start balancing
+    while(abs(imu.get_acc_z()) > 10):
+        pass
+
+    # Inform user that we're starting to balance
+    print("Starting to balance!")
+    board.set_rgb_led(0,0,64)
+
+    # Get current pitch of robot. We know that we're close to the balancing
+    # point, so this represents the approximate zero pitch angle of the robot
+    angle_offset = imu.get_pitch()
+
+    # Reset encoder values to zero, this is our initial target position to hold
+    drivetrain.reset_encoder_position()
+
+    # Reset PID controllers
+    angle_pid.clear_history()
+    position_pid.clear_history()
+
+    # Reset other values
+    target_position = 0
+    target_speed = 0
+    turn_effort = 0
+    
+    # Reset loop timer
+    last_loop_time = time.ticks_ms()
+
+    # Balance loop
+    while(True):
+        # Wait until the loop period has passed to ensure the loop runs at a
+        # consistent rate
+        if(time.ticks_ms() < last_loop_time + (loop_period * 1000)):
+            continue
+
+        # Update loop timer
+        last_loop_time += loop_period * 1000
+
+        # Update sensor values
+        angle = imu.get_pitch() - angle_offset
+        left_position = drivetrain.get_left_encoder_position()
+        right_position = drivetrain.get_right_encoder_position()
+
+        # Compute robot position by averaging the left and right wheel positions
+        bot_position = (left_position + right_position) / 2
+
+        # Update target position based on target velocity
+        target_position -= target_speed * loop_period
+
+        # Compute target angle from robot's position with the PID controller
+        targetAngle = position_pid.update(target_position - bot_position)
+
+        # Compute forward effort from robot's angle with the PID controller
+        forwardEffort = angle_pid.update(targetAngle - angle)
+
+        # Set motor effort
+        drivetrain.arcade(forwardEffort, turn_effort)
+
+        # Check the angle and forward effort. If either is too large, the robot
+        # has likely fallen over, so stop the motors
+        if(abs(angle) > 45 or abs(forwardEffort) > 5):
+            print("Effort too large, stopping!")
+            drivetrain.stop()
+            break
